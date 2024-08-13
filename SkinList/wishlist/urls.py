0a69537a9b2d6ddf160dfa6ed0431be0999a2d99
Camilo <cdisidoro99@gmail.com>
2024-08-13 11:20:15 +0200

    Shop as new Home

--- SkinList/wishlist/urls.py
+++ SkinList/wishlist/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.shop, name="index"),
     path("shop", views.shop, name="shop"),
     path("cosmetics", views.cosmetics, name="cosmetics"),
     path("bundles", views.bundles, name="bundles"),
